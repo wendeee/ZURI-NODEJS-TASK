9674d5aa8aa5725fbbb437ac940af8351473d696
unknown <chinwe.promise2016@gmail.com>
2021-04-15 21:26:18 -0700

    Modify script file to check is directory exist before writing to file

--- script.js
+++ script.js
@@ -7,22 +7,24 @@ const fs = require('fs')
 const url = 'https://jsonplaceholder.typicode.com/posts';
 
 //Use https get method to fetch data
-https.get(url, res =>{
+https.get(url, res => {
     let data = '';
     res.on('data', chunk => {
-        data += chunk
+        data +=chunk
     })
-    res.on('end', () => {
+    res.on('end', () =>{
         (data)
-        fs.writeFile('result/posts.json', data, (err) => {
-            if(err) throw err
-            console.log('File added successfully')
-        })  
-    })  
+        let dir = './result';
+        if(fs.access(dir, (err)=>{
+            console.log(`Directory ${err ? 'does not exist': 'exists'}`)
+            fs.writeFile('result/posts.json', data, (err) => {
+                if(err) throw err
+                console.log('File written successfully')
+            })
+        })){
+
+        }
+    })
 }).on('error', err => {
     console.log(err.message)
 })
-
-
-
-
